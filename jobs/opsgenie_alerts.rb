8ce3b569c6d9a50c0049380d73f93acc00d509a3
Nick Badger <nbadger@mintel.com>
2019-02-13 19:57:10 +0000

    Switch to hotness widget.

--- jobs/opsgenie_alerts.rb
+++ jobs/opsgenie_alerts.rb
@@ -10,14 +10,19 @@ SCHEDULER.every '30s', :first_in => 0 do |job|
   end
 
   alerts = JSON.parse(response.body)['data']
+  puts "GOT"
+  puts alerts
 
-  open_alerts   = alerts.count
+
+  open_alerts   = 0 # alerts.count
   unseen_alerts = 0
   unack_alerts  = 0
   p1_alerts     = 0
   p2_alerts     = 0
   p3_alerts     = 0
 
+  puts alerts
+  
   alerts.each do |a|
     if a["isSeen"] == false
       unseen_alerts +=1
@@ -38,10 +43,10 @@ SCHEDULER.every '30s', :first_in => 0 do |job|
     end
   end
 
-  send_event('opsgenie_open', { title: "Open", value: open_alerts })
-  send_event('opsgenie_unseen', { title: "UnSeen", value: unseen_alerts })
-  send_event('opsgenie_unack', { title: "UnAck", value: unack_alerts })
-  send_event('opsgenie_p1', { title: "P1", value: p1_alerts })
-  send_event('opsgenie_p2', { title: "P2", value: p2_alerts })
-  send_event('opsgenie_p3', { title: "P3", value: p3_alerts })
+  send_event('opsgenie_open', text: open_alerts)
+  send_event('opsgenie_unseen', text: unseen_alerts)
+  send_event('opsgenie_unack', text: unack_alerts)
+  send_event('opsgenie_p1', text: p1_alerts)
+  send_event('opsgenie_p2', text: p2_alerts)
+  send_event('opsgenie_p3', text: p3_alerts)
 end
